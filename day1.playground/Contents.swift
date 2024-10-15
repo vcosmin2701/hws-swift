c708d24c8015b8a21a237bf9154acbad5cb9284d
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-15 14:21:10 +0300

    variables and constants in Swift

---
+++ day1.playground/Contents.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+// --------Variables & Constants-------------------
+
+var businessName = "Agile"
+businessName = "Agile Freaks"
+
+print(businessName)
+
+let bestTeam = "iOS Team"
+// bestTeam = "error" -> this will cause an error
+
+
+
