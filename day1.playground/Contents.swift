dd91dc48d2bd6be679473e519129025577ed2bf1
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-15 14:52:19 +0300

    numbers in swift

--- day1.playground/Contents.swift
+++ day1.playground/Contents.swift
@@ -27,3 +27,33 @@ print(firstDay.hasPrefix("I st"))
 
 // check if the string ends with some sequence of characters
 print(firstDay.hasSuffix("amazing"))
+
+// ------------------Numbers-----------------------
+
+let points = 1000
+let mageWinPoints = 1_000_000
+
+// Basic math operations
+let higherScore = points + 1000
+let doubledScore = points * 2
+let squaredScore = points * points
+let halvedScore = points / 2
+
+// short-hand operations
+var count = 10
+count += 5
+count *= 5
+count /= 10
+count -= 2
+
+// Check if a number is multiple of another number
+let number = 120
+print(number.isMultiple(of: 4))
+
+// decimal numbers, aka floating point
+
+let a = 1
+let b = 2.0
+// let c = a + b -> error: can't mix int with double
+let c = Double(a) + b
+print(c)
