7a1fdd7811d41904dcdf848056b0e98dc853aae3
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-15 14:35:10 +0300

    strings in swift

--- day1.playground/Contents.swift
+++ day1.playground/Contents.swift
@@ -10,5 +10,20 @@ print(businessName)
 let bestTeam = "iOS Team"
 // bestTeam = "error" -> this will cause an error
 
+// --------------------Strings-------------------
 
+let firstDay = """
+I started learning Swift
+I'm iOS Intern now :D
+"""
 
+print(firstDay)
+
+// length of the string
+print(firstDay.count)
+
+// check if the string starts with some sequence of characters
+print(firstDay.hasPrefix("I st"))
+
+// check if the string ends with some sequence of characters
+print(firstDay.hasSuffix("amazing"))
