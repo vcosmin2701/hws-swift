fc344f76337fd9d072d89c52bebe807d15f63fc5
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-17 14:54:30 +0300

    While loops in Swift

--- day2_3.playground/Contents.swift
+++ day2_3.playground/Contents.swift
@@ -37,3 +37,27 @@ for _ in 1...5 {
     repeatMessage += repeatMessage
 }
 print(repeatMessage)
+
+// -------While loop-------------
+
+
+var countdown = 10
+
+while countdown > 0 {
+    print("\(countdown)...")
+    countdown -= 1
+}
+
+print("Blast off!")
+
+let id = Int.random(in: 1...100)
+let amount = Double.random(in: 0...1)
+
+var roll = 0
+
+while roll != 20 {
+    roll = Int.random(in: 1...20)
+    print("I rolled a \(roll)")
+}
+
+print("Critical hit!")
