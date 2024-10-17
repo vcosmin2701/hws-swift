508a2bfc0dcc92af2922f076d29771d3faf2a231
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-17 14:09:59 +0300

    Switch statement in Swift

--- day2_2.playground/Contents.swift
+++ day2_2.playground/Contents.swift
@@ -16,7 +16,35 @@ if transport == .bike {
     print("I think you can go with a car or walk")
 }
 
+// -------- Switch statement -----------
+
+enum Weather {
+    case sun, rain, wind, snow
+}
+
+var forecast: Weather = .sun
+
+switch forecast {
+case .sun:
+    print("It should be a nice day.")
+case .rain:
+    print("Pack an umbrella")
+case .wind:
+    print("Wear something warm")
+case .snow:
+    print("University is closed, free time.")
+}
 
 
+var animal = "Chinchilla"
 
-
+switch animal {
+case "Chinchilla":
+    print("Stitch")
+    fallthrough
+case "Dog":
+    print("It can bark")
+    fallthrough
+default:
+    print("Something cause it to be like this")
+}
