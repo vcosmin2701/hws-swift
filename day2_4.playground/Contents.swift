4a5d64a9050a102f81d8fa54baff114104bd88b4
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-18 09:49:37 +0300

    Return values from functions

--- day2_4.playground/Contents.swift
+++ day2_4.playground/Contents.swift
@@ -24,3 +24,33 @@ func printTimesTables(number: Int, end: Int){
 printTimesTables(number: 10, end: 20)
 
 
+// ------ Return values from functions
+
+let root = sqrt(169)
+print(root)
+
+print("ceva")
+
+func rollDice() -> Int {
+    return Int.random(in: 1...6)
+}
+
+let result = rollDice()
+print(result)
+
+// Exercise
+
+func sameLetters(string1: String, string2: String) -> Bool {
+    string1.sorted() == string2.sorted()
+}
+
+print(sameLetters(string1: "car", string2: "rac"))
+
+func pythagoras(a: Double, b: Double) -> Double {
+    sqrt(a * a + b * b)
+}
+
+let c = pythagoras(a: 3, b: 4)
+print(c)
+
+
