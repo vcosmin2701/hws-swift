2844ca76cc0c05004ec38a7baa981db2bc5ec7eb
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-22 09:14:29 +0300

    Property observers

--- day4.playground/Contents.swift
+++ day4.playground/Contents.swift
@@ -68,3 +68,38 @@ print(archer1.vacationRemaining)
 archer1.vacationRemaining = 5
 print(archer1.vacationAllocated)
 
+// --------- Property observers ----------------
+
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+
+game.score += 10
+game.score -= 3
+game.score += 1
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+        
+        didSet {
+            print("There are now \(contacts.count) contacts")
+            print("Old value was: \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Cosmo")
+app.contacts.append("Alieno")
+app.contacts.append("Ish")
