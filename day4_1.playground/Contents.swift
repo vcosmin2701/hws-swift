72bfbad3eab7fd4e369afc2cc4e115a46f264539
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-22 11:33:44 +0300

    Basic class inheritance in Swift

--- day4_1.playground/Contents.swift
+++ day4_1.playground/Contents.swift
@@ -12,3 +12,41 @@ class Game {
 
 var newGame = Game()
 newGame.score += 10
+
+// Inheritance
+
+class Employee {
+    let hours: Int
+    
+    init(hours: Int) {
+        self.hours = hours
+    }
+    
+    func printSummary() {
+        print("I work \(hours) hours a day.")
+    }
+}
+
+class Developer: Employee {
+    func work() {
+        print("I'm writing code for \(hours) hours")
+    }
+    
+    override func printSummary() {
+        print("I work \(hours) hours a day cuz I'm a developer")
+    }
+}
+
+class Manager: Employee {
+    func work() {
+        print("I'm going to meetings for \(hours) hours")
+    }
+}
+
+let cosmo = Developer(hours: 8)
+let jojo = Manager(hours: 10)
+let pufarina = Developer(hours: 2)
+
+cosmo.work()
+jojo.work()
+pufarina.printSummary()
