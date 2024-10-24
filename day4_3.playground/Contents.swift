105957c80de4799e1f1d3ee98f3502ce5f076ed4
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-24 09:25:36 +0300

    Unwrap optionals: guard, nil coalescing

--- day4_3.playground/Contents.swift
+++ day4_3.playground/Contents.swift
@@ -36,3 +36,69 @@ if let unwrappedNumber = number {
 } else {
     print("No number found at all")
 }
+
+// ----- unwrap optionals with guard ---------
+
+func printSquare(of number: Int?) {
+    guard let number = number else {
+        print("Missing input")
+        return
+    }
+    print("\(number) x \(number) is \(number * number)")
+}
+
+var num: Int? = 3
+printSquare(of: num)
+
+var myVar: Int? = 3
+
+if let unwrapped = myVar {
+    print("Run if myVar has a value inside")
+}
+
+//guard let unwrapped = myVar else {
+//    print("Run if myVar doesn't have a value")
+//}
+
+func addScore(addWith points: Int?) {
+    guard let pointsAdd = points else {
+        print("No score to add")
+        return
+    }
+    print("\(pointsAdd) points will be added to the score")
+}
+
+addScore(addWith: nil)
+
+// nil coalescing
+
+let captains = [
+    "Enterprise": "Picard",
+    "Voyager": "Janeway",
+    "Defiant": "Sisko"
+]
+
+let new = captains["Serenity"] ?? "N/A"
+print(new)
+
+//let tvShows = [String]()
+let tvShows = ["Archer", "Babylon 5", "Ted Lasso"]
+
+for _ in 1...20 {
+    let favorite = tvShows.randomElement() ?? "None"
+    print(favorite)
+}
+
+struct Book {
+    let title: String
+    let author: String?
+}
+
+let book = Book(title: "Beowulf", author: nil)
+let author = book.author ?? "Anonymous"
+print(author)
+
+let input = ""
+let num1 = Int(input) ?? 0
+print(num1)
+
